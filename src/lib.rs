af47d71670510a2cace52fe138006dcafe102283
b0xtch <botchm@hotmail.com>
2025-01-25 16:18:26 +0200

    chore: upgrade deps

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,12 @@
 
 #![allow(proc_macro_derive_resolution_fallback)]
 
-#[macro_use] extern crate diesel;
+#[macro_use]
+extern crate diesel;
 extern crate postgis;
 #[cfg(feature = "serde")]
-#[macro_use] extern crate serde;
+#[macro_use]
+extern crate serde;
 
 pub mod sql_types;
 pub mod types;
